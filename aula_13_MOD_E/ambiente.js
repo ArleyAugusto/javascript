9872314c3f23cdeb325dfb9e6ea6bd3e79250c9d
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-09 21:00:33 -0300

    Aula 13 - Repetições_pt1

---
+++ aula_13_MOD_E/ambiente.js
@@ -0,0 +1,30 @@
+/* DO */
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c +1
+}   while (c <= 2)
+
+
+__________________________
+
+/* 
+WHILE ********
+
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++ // c = c +1
+}
+*/
+
+_____________________________
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
