b0e7877d5fb895b48a336bf5c72de8809e1b359f
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-16 15:45:06 -0300

    Resumao_basico_JS

--- Praticando_JS/assets/script.js
+++ Praticando_JS/assets/script.js
@@ -1,21 +1,22 @@
-//TIPOS DE VARIAVEIS
-let idade = 90 // Number
-let nome = 'Arley' // String
-// OBS: Usar '' ou "" em operacao ex? let nome = "90"
-//Logo em op console.log(10 + nome) será 1090 pois ira concatenar (será considerado como texto)
-let logado =  true // Boolean - conjunção, disjunção, disjunção exclusiva, equivalência lógica e negação
+/*
+TIPOS DE VARIAVEIS
+let idade = 18 // Number
+let nome = 'joao' // String
+ OBS: Usar '' ou "" em operacao ex? let nome = "90"
+Logo em op console.log(10 + nome) será 1090 pois ira concatenar (será considerado como texto)
+let logado =  true Boolean - conjunção, disjunção, disjunção exclusiva, equivalência lógica e negação
 
-//___________________________________________________
+___________________________________________________
 
 
-// Array e Objetos
+Array e Objetos
 
-let ingredientes = ['farinha', 'agua', 'corante', 'sal'] //array
+let ingredientes = ['farinha', 'agua', 'corante', 'sal'] -- array
 
-//array - listas /Caso queria adicionar varios itens dentro do array basta adicionar []. Quando quiser exibir determinado item basta colocar o numero sequancial na qual foi inserido. Ex sal, farinha, ovo = 0, 1, 2
-// Exibir sal console.log(ingredientes[0])
+array - listas /Caso queria adicionar varios itens dentro do array basta adicionar []. Quando quiser exibir determinado item basta colocar o numero sequancial na qual foi inserido. Ex sal, farinha, ovo = 0, 1, 2
+ Exibir sal console.log(ingredientes[0])
 
-//___________________________________________________
+___________________________________________________
 
 let personagem = { // Objetos 
     nome: 'Arley',
@@ -23,22 +24,21 @@ let personagem = { // Objetos
     forca: 800,
     magia: 1000,
     mana: 200 
-}
+}  
 
-// adicionar propriedades (caracteristicas de um objeto)
-//exemplos <<<
-//console.log(personagem.nome) <<<<
-//console.log(personagem.magia) <<<
+adicionar propriedades (caracteristicas de um objeto)
+exemplos <<<
+console.log(personagem.nome) <<<<
+console.log(personagem.magia) <<<
 
-//___________________________________________________
+___________________________________________________
+FUNCOES <<<<<
 
-// FUNCOES <<<<<
+INPUT -- Dados que vou mandar pra funcao 
+PROCESSA -- Processamento
+OUTPUT -- Retorno da funcao
 
-// INPUT -- Dados que vou mandar pra funcao 
-//PROCESSA -- Processamento
-//OUTPUT -- Retorno da funcao
-
-/*function somar(a, b) {
+function somar(a, b) {
     let resultado = a + b;
     return resultado
 }
@@ -47,19 +47,19 @@ let x = somar(15, 5)
 let y = somar (2, 8)
 
 console.log(x)
-console.log(y) */
+console.log(y) 
 
 
-// ----------------------------
+ ----------------------------
 
-//FUNCAO ARROW 
+FUNCAO ARROW 
 
-/*function somar(a, b) {
+function somar(a, b) {
     let resultado = a + b
     return resultado
 }
 
-let somar2 = (a, b) => {  <<<<<<<<<<<
+let somar2 = (a, b) => {  <<<<<<<<<<< arrow function
     let resultado = a + b
     return resultado
 }
@@ -68,6 +68,41 @@ let x = somar(15, 5)
 let y = somar2(2, 8) <<<<<<<
 
 console.log(x)
-console.log(y)  <<<<<<<<<<<<< */ 
+console.log(y)  <<<<<<<<<<<<<  
 
- // serve para mandar uma funcao para algum lugar sem necessariamente dar um nome a ela
+  serve para mandar uma funcao para algum lugar sem necessariamente dar um nome a ela
+
+
+
+  CONDICIONAL
+
+ 
+
+if (nome == 'arley') { // SE
+    console.log("Mensagem especial!!")
+} 
+
+else {    // SE NÃO
+    console.log ('Mensagem normal')
+}
+
+
+____________________________
+
+
+== igual
+!= diferente
+> Maior
+< Menor
+>= <= maior igual, menor igual
+
+______________________________
+
+
+ let lista = ['farinha', 'agua', 'sal']
+
+ for (let item of lista) {
+    console.log('-  Ingredientes: ' + item)
+ }
+
+*/
