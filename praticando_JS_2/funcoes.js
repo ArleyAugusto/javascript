365c975b9cb2eb221ea66fac4a1d34b7f7476025
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-19 21:05:01 -0300

    Reforço Funções

---
+++ praticando_JS_2/funcoes.js
@@ -0,0 +1,28 @@
+/* Mais estudos rel funções
+   Trecho de codigo que so é executado quando é chamado/invocado.
+
+   Funcao Void (vazia)
+   Funcao com Paraametro(s)
+   Funcao Return
+   Funcao Arro /  Arrow Function
+   
+   
+
+let valor = 20
+
+ function soma(numero1, numero2) {
+    const somaDosNumeros = numero1 + numero2
+
+    return somaDosNumeros
+ }
+
+const meusNumeros = soma(4,6)
+
+console.log(meusNumeros)*/
+
+const multi = (numero1, numero2) => numero1 * numero2
+
+
+const multiplicacao = multi(2,5)
+
+console.log(multiplicacao)
