6b3bf7acf2d7a110f74c44f5c67b2f3fce87c875
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-27 21:42:01 -0300

    Aula_16 (Exercicios funções)

---
+++ aula_16/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if(n%2 == 0) {
+        return 'Par!!'
+    } else {
+        return 'Impar!!'
+    }
+}
+
+var res = parimpar(5) // 5 = o parametro  
+console.log(res)
+
+// ou somente  console.log(parimpar(4))
---
+++ aula_16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) {  // adicionando parametros caso mande mostar somente um valor ele nao trata NaN e sim retornara o valor n1 ou n2
+    return n1 + n2
+}
+
+console.log(soma(2,5))
